8cc71eee4fcefd85a536c8f9cd7c910c0a93b995
vipul <vipulaery@gmail.com>
2021-09-14 21:35:39 +0530

    added logic for adding two valid numbers and valid result.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,20 @@
+import static java.util.Objects.isNull;
+
 public class StringCalculator {
+    private final String SPLITTER = ",";
     public int add(String numbers) {
-        return 0;
+        if(isNull(numbers) || numbers.isEmpty()) {
+            return 0;
+        }
+
+        String[] numbersArray = numbers.split(SPLITTER);
+        int number1 = Integer.parseInt(numbersArray[0]);
+        if(numbersArray.length == 1) {
+            return number1;
+        }
+        int number2 = Integer.parseInt(numbersArray[1]);
+
+        return number1 + number2;
     }
 
 }
