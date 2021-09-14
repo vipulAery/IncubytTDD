1845caf2085bbeddf4002590051b5a0758f11a76
vipul <vipulaery@gmail.com>
2021-09-14 20:35:11 +0530

    added testacses for 1 or 2 numbers

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -8,4 +8,49 @@ public class StringCalculatorTest {
     void addEmptyString() {
         assertEquals(0, stringCalculator.add(""));
     }
+
+    @Test
+    void addSingleNumberString() {
+        assertEquals(1, stringCalculator.add("1"));
+    }
+
+    @Test
+    void addTwoDigitSingleNumberString() {
+        assertEquals(12, stringCalculator.add("12"));
+    }
+
+    @Test
+    void addTwoNumbersString() {
+        assertEquals(4, stringCalculator.add("1,3"));
+    }
+
+    @Test
+    void addTwoDigitTwoNumberString() {
+        assertEquals(62, stringCalculator.add("12,50"));
+    }
+
+    @Test
+    void addNegativeSingleNumberString() {
+        assertEquals(-13, stringCalculator.add("-13"));
+    }
+
+    @Test
+    void addBothNegativeTwoNumberString() {
+        assertEquals(-25, stringCalculator.add("-13,-12"));
+    }
+
+    @Test
+    void addFirstNegativeTwoNumberString() {
+        assertEquals(-1, stringCalculator.add("-13,12"));
+    }
+
+    @Test
+    void addSecondNegativeTwoNumberString() {
+        assertEquals(-1, stringCalculator.add("12,-13"));
+    }
+
+    @Test
+    void addSmallNegativeTwoNumberString() {
+        assertEquals(1, stringCalculator.add("13,-12"));
+    }
 }
