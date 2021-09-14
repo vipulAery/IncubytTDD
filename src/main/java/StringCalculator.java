20f187dbf9d4216b2a5a8531c6544502f4a748e5
vipul <vipulaery@gmail.com>
2021-09-15 02:41:07 +0530

    added logic for custom delimiter

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,15 +2,15 @@ import static java.util.Objects.isNull;
 
 public class StringCalculator {
     private final String SPLITTER_FORMAT = "[,\n%s]+";
+    private final int STARTING_INDEX_OF_NUMBERS = 4;
 
     public int add(String numbers) {
         if (isNull(numbers) || numbers.isEmpty()) {
             return 0;
         }
 
-        boolean customSplitterAvailable = isCustomSplitterAvailable(numbers);
 
-        String[] numbersArray = getNumbersArray(numbers, customSplitterAvailable);
+        String[] numbersArray = getNumbersArray(numbers);
         int sum = 0;
         int number;
         for (String s : numbersArray) {
@@ -21,16 +21,17 @@ public class StringCalculator {
         return sum;
     }
 
-    private String[] getNumbersArray(String numbers, boolean customSplitterAvailable) {
+    private String[] getNumbersArray(String numbers) {
+        boolean customSplitterAvailable = isCustomSplitterAvailable(numbers);
         String splitAt = getSplitter(customSplitterAvailable, numbers);
-        if(customSplitterAvailable) {
-            numbers = numbers.substring(4);
+        if (customSplitterAvailable) {
+            numbers = numbers.substring(STARTING_INDEX_OF_NUMBERS);
         }
         return numbers.split(splitAt);
     }
 
     private String getSplitter(boolean customSplitterAvailable, String numbers) {
-        return String.format(SPLITTER_FORMAT, customSplitterAvailable?numbers.charAt(2):"");
+        return String.format(SPLITTER_FORMAT, customSplitterAvailable ? numbers.charAt(2) : "");
     }
 
     public boolean isCustomSplitterAvailable(String numbers) {
