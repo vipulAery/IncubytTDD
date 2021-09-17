cb23afa561850cdb89f469308de0148af7ce2dc8
vipul <vipulaery@gmail.com>
2021-09-17 23:08:37 +0530

    refactored unit testcase

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,119 +1,89 @@
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
 public class StringCalculatorTest {
     StringCalculator stringCalculator = new StringCalculator();
 
-
-    @Test
-    void addEmptyString() {
-        assertEquals(0, stringCalculator.add(""));
-    }
-
-    @Test
-    void addSingleNumberString() {
-        assertEquals(1, stringCalculator.add("1"));
-    }
-
-    @Test
-    void addTwoDigitSingleNumberString() {
-        assertEquals(12, stringCalculator.add("12"));
-    }
-
-    @Test
-    void addTwoNumbersString() {
-        assertEquals(4, stringCalculator.add("1,3"));
-    }
-
-    @Test
-    void addTwoDigitTwoNumberString() {
-        assertEquals(62, stringCalculator.add("12,50"));
-    }
-
-    @Test
-    void addNegativeSingleNumberStringThrowsException() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> stringCalculator.add("-13"));
-        assertEquals("negatives not allowed : -13", exception.getMessage());
-    }
-
-    @Test
-    void addSingleLongNumberString() {
-        assertThrows(NumberFormatException.class, () -> stringCalculator.add("123456789012345"));
-    }
-
-    @Test
-    void addTwoNumbersStringContainingLongNumberThrowsException() {
-        assertThrows(NumberFormatException.class, () -> stringCalculator.add("12345,2147483648"));
-    }
-
-    @Test
-    void addTwoNumbersStringContainingIntgerMaxValue() {
-        assertEquals(0, stringCalculator.add("2147483647"));
-    }
-
-    @Test
-    void addTwoNumbersStringContainingIntgerMaxValueWithIntgerOverFlowThrowsException() {
-        assertThrows(Exception.class, () -> stringCalculator.add(getTestDataForIntegerOverFlow()));
-
-    }
-
-    private String getTestDataForIntegerOverFlow() {
-        StringBuilder stringBuilder = new StringBuilder(90000000);
-        stringBuilder.append("999,1000");
-        for (int i = 0; i < 10000000; i++) {
-            stringBuilder.append(",999,1000");
+    @Nested
+    @DisplayName("When the numbers are valid")
+    class ValidNumbers {
+        @ParameterizedTest(name = "ExpectedSum = {0}, numbers = {1}")
+        @CsvSource({
+                "0, ''",
+                "1, 1",
+                "12, 12",
+                "4, '1,3'",
+                "62, '12,50'",
+                "13, '1,3,4,5'",
+                "0, '2147483647'"
         }
-        return stringBuilder.toString();
+        )
+        @DisplayName("add numbers having default delimiters")
+        void addNumbers(int expectedSum, String numbers) {
+            assertEquals(expectedSum, stringCalculator.add(numbers));
+        }
+
+        @ParameterizedTest(name = "ExpectedSum = {0}, numbers = {1}")
+        @CsvSource({
+                "19, '1\n3\n4,5,6'",
+                "19, '//[;]\n1;3;4;5;6'",
+                "19, '//[\t\t\t]\n1\n3\t\t4,5\t\t6'",
+                "26, '//[,,,]\n1,3,,,4,5,,,6,7'",
+                "14, '//[\t]\n1\n3\n4,1001\t6'",
+                "30, '//[\t][;][*]\n1\n3;4,1001\t6\t9*7'",
+                "30, '//[\t][\t\t][**]\n1\n3\n4,1001\t6\t\t9**7'"
+        }
+        )
+        @DisplayName("add numbers having custom delimiters")
+        void addNumbersWithCustomDelimiters(int expectedSum, String numbers) {
+            assertEquals(expectedSum, stringCalculator.add(numbers));
+        }
     }
 
-    @Test
-    void addMultipleNumbersString() {
-        assertEquals(13, stringCalculator.add("1,3,4,5"));
+    @Nested
+    @DisplayName("When the numbers are invalid")
+    class InvalidNumbers {
+        @Test
+        void addNegativeSingleNumberStringThrowsException() {
+            Exception exception = assertThrows(IllegalArgumentException.class, () -> stringCalculator.add("-13"));
+            assertEquals("negatives not allowed : -13", exception.getMessage());
+        }
+
+        @Test
+        void addSingleLongNumberString() {
+            assertThrows(NumberFormatException.class, () -> stringCalculator.add("123456789012345"));
+        }
+
+        @Test
+        void addTwoNumbersStringContainingLongNumberThrowsException() {
+            assertThrows(NumberFormatException.class, () -> stringCalculator.add("12345,2147483648"));
+        }
+
+        @Test
+        void addTwoNumbersStringContainingIntgerMaxValueWithIntgerOverFlowThrowsException() {
+            assertThrows(Exception.class, () -> stringCalculator.add(getTestDataForIntegerOverFlow()));
+        }
+
+        private String getTestDataForIntegerOverFlow() {
+            StringBuilder stringBuilder = new StringBuilder(90000000);
+            stringBuilder.append("999,1000");
+            for (int i = 0; i < 10000000; i++) {
+                stringBuilder.append(",999,1000");
+            }
+            return stringBuilder.toString();
+        }
+
+        @Test
+        void addMultipleNumbersStringWithNegativeAndPositiveIntegersThrowsException() {
+            Exception exception = assertThrows(IllegalArgumentException.class, () -> stringCalculator.add("1,-3,4,-5"));
+            assertEquals("negatives not allowed : -3 -5", exception.getMessage());
+        }
     }
-
-    @Test
-    void addMultipleNumbersStringWithNegativeAndPositiveIntegersThrowsException() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> stringCalculator.add("1,-3,4,-5"));
-        assertEquals("negatives not allowed : -3 -5", exception.getMessage());
-    }
-
-    @Test
-    void addMultipleNumbersStringWithTwoTypesOfDelimiters() {
-        assertEquals(19, stringCalculator.add("1\n3\n4,5,6"));
-    }
-
-    @Test
-    void addMultipleNumbersStringWithCustomDelimiter() {
-        assertEquals(19, stringCalculator.add("//[;]\n1;3;4;5;6"));
-    }
-
-    @Test
-    void addMultipleNumbersStringWithCustomAsWellDefaultDelimiters() {
-        assertEquals(19, stringCalculator.add("//[\t\t\t]\n1\n3\t\t4,5\t\t6"));
-    }
-
-    @Test
-    void addMultipleNumbersStringWithCustomAsWellDefaultDelimiters1() {
-        assertEquals(26, stringCalculator.add("//[,,,]\n1,3,,,4,5,,,6,7"));
-    }
-
-
-    @Test
-    void addMultipleNumbersStringContainingNumberGreaterThan1000() {
-        assertEquals(14, stringCalculator.add("//[\t]\n1\n3\n4,1001\t6"));
-    }
-
-    @Test
-    void addMultipleNumbersStringWithMultipleSingleCharCustomDelimitersAsWellDefaultDelimiters() {
-        assertEquals(30, stringCalculator.add("//[\t][;][*]\n1\n3;4,1001\t6\t9*7"));
-    }
-
-    @Test
-    void addMultipleNumbersStringWithMultipleMultiCharCustomDelimitersAsWellDefaultDelimiters() {
-        assertEquals(30, stringCalculator.add("//[\t][\t\t][**]\n1\n3\n4,1001\t6\t\t9**7"));
-    }
-
 }
